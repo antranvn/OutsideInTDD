353e9630b0d5da80f84446a785da9bfe72ca0026
antranvn <antran.cmc@gmail.com>
2022-07-06 14:04:18 +0700

    feat: add coroutines and ktlint

--- app/src/main/java/com/example/outsidein/Engine.kt
+++ app/src/main/java/com/example/outsidein/Engine.kt
@@ -1,10 +1,16 @@
 package com.example.outsidein
 
-class Engine(var temperature: Int = 15, var isTurnedOn: Boolean = false) {
+import android.util.Log
+
+class Engine(var temperature: Int = 15,
+             var isTurnedOn: Boolean = false) {
 
 
     fun turnOn() {
         isTurnedOn = true
+        Thread.sleep(6000)
         temperature = 95
+
+        Log.d("COURSE", "Engine has turned on")
     }
 }
